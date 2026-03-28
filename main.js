7e0da0924cc34df87a2518f58e1b03d018d14ada
cabatu-9e-ay2526 <cabatu.mh@obmontessori.edu.ph>
2026-03-28 14:36:03 +0800

    main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ function checkAge() {
 
     let age = document.getElementById("age").value;
 
-    // Convert input to number
     if (Number(age) >= 18) {
         document.getElementById("answer").innerHTML = "Status: You're eligible to vote";
     }
